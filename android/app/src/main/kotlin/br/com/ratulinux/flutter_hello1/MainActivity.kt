ed6e29c7a6fb42753f1d2fbc247eebce56aff4a3
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-02-19 16:19:55 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/ratulinux/flutter_hello1/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.ratulinux.flutter_hello1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Flutter"),
+        centerTitle: true,
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: _img(),
+      ),
+    );
+  }
+
+  _text() {
+    return Text(
+      "Hello World!",
+      style: TextStyle(
+          fontSize: 30,
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic,
+          decoration: TextDecoration.underline,
+          decorationColor: Colors.red,
+          decorationStyle: TextDecorationStyle.wavy),
+    );
+  }
+
+  _img() {
+    return Image.asset(
+      "assets/images/dog1.png",
+      width: 300,
+      height: 300,
+      fit: BoxFit.cover,
+    );
+  }
+}
