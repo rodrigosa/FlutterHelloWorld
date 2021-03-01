0d88d6a9d42bd40532584e97b3e547e06fa7058e
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-03-01 17:28:19 -0300

    Button

---
+++ lib/pages/hello_page1.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Hellopage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Page 1"),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hello1/pages/hello_page1.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -7,52 +7,101 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Hello Flutter"),
-        centerTitle: true,
       ),
-      body: _body(),
+      body: _body(context),
     );
   }
 
-  _body() {
+  _body(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Center(
-        child: _button(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _text(),
+          _pageView(),
+          _buttons(context)
+        ],
       ),
+    );
+  }
+
+  Container _pageView() {
+    return Container(
+      margin: EdgeInsets.only(top: 20, bottom: 20),
+          height: 300,
+          child: PageView(
+            children: [
+              _img("assets/images/dog1.png"),
+              _img("assets/images/dog2.png"),
+              _img("assets/images/dog3.png"),
+              _img("assets/images/dog4.png"),
+              _img("assets/images/dog5.png")
+            ],
+          ),
+        );
+  }
+
+  _buttons(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _button(context, "ListView"),
+            _button(context, "Page 2"),
+            _button(context, "Page 3")
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _button(context, "Snack"),
+            _button(context, "Dialog"),
+            _button(context, "Touch")
+          ],
+        ),
+      ],
+    );
+  }
+
+  _button(BuildContext context, String text) {
+    return ElevatedButton(
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        onPressed: () => _onClickOk(context)
+    );
+  }
+
+  void _onClickOk(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context){
+      return Hellopage1();
+    }));
+  }
+
+  _img(String img) {
+    return Image.asset(
+      img,
+      fit: BoxFit.cover,
     );
   }
 
   _text() {
     return Text(
-      "Hello World!",
+      "Hello World",
       style: TextStyle(
-          fontSize: 30,
           color: Colors.blue,
+          fontSize: 30,
           fontWeight: FontWeight.bold,
           fontStyle: FontStyle.italic,
           decoration: TextDecoration.underline,
           decorationColor: Colors.red,
           decorationStyle: TextDecorationStyle.wavy),
-    );
-  }
-
-  _button() {
-    return ElevatedButton(
-      child: Text("OK"),
-      onPressed: () => _onclickOK(),
-    );
-  }
-
-  void _onclickOK(){
-    print("Clicou no botão!");
-  }
-
-  _img() {
-    return Image.asset(
-      "assets/images/dog1.png",
-      width: 300,
-      height: 300,
-      fit: BoxFit.cover,
     );
   }
 }
