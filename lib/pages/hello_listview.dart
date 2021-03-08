ac1326eb134770080678d35c9360b0a35c9d922a
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-03-08 17:39:17 -0300

    Atualização de tela grid e list stateful com o setState

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -8,7 +8,14 @@ class Dog {
   Dog(this.nome, this.foto);
 }
 
-class HelloListView extends StatelessWidget {
+class HelloListView extends StatefulWidget {
+  @override
+  _HelloListViewState createState() => _HelloListViewState();
+}
+
+class _HelloListViewState extends State<HelloListView> {
+  bool _gridView = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +26,17 @@ class HelloListView extends StatelessWidget {
               icon: Icon(Icons.list),
               onPressed: () {
                 print("Lista");
+                setState(() { // Responsável por fazer o Flutter Redesenhar o widget na tela
+                  _gridView = false;
+                });
               }),
           IconButton(
               icon: Icon(Icons.grid_on),
               onPressed: () {
                 print("Grid");
+                setState(() {
+                  _gridView = true;
+                });
               })
         ],
       ),
@@ -40,40 +53,54 @@ class HelloListView extends StatelessWidget {
       Dog("Pastor", "assets/images/dog5.png")
     ];
 
-    return GridView.builder(
-      itemCount: dogs.length,
-      gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (context, index) {
-        Dog dog = dogs[index];
-        return Stack(
-          fit: StackFit.expand,
+    if (_gridView) {
+      return GridView.builder(
+        itemCount: dogs.length,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (context, index) {
+          return _itemView(dogs, index);
+        },
+      );
+    } else {
+      return ListView.builder(
+        itemCount: dogs.length,
+        itemExtent: 350,
+        itemBuilder: (context, index) {
+          return _itemView(dogs, index);
+        },
+      );
+    }
+  }
 
-          /// Pode-se usar também o SizedBox.expand()
-          children: [
-            _img(dog.foto),
+  _itemView(List<Dog> dogs, int index) {
+    Dog dog = dogs[index];
+    return Stack(
+      fit: StackFit.expand,
 
-            /// Nesta ordem a foto fica por baixo do nome
-            Container(
-              /// Ao inv[es de Container pode-se usar também o widget Align, porém o Container possui mais recursos
-              alignment: Alignment.topLeft,
-              child: Container(
-                margin: EdgeInsets.all(12),
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
+      /// Pode-se usar também o SizedBox.expand()
+      children: [
+        _img(dog.foto),
 
-                    /// O Black45 possui 45% de opacidade
-                    color: Colors.black45,
-                    borderRadius: BorderRadius.circular(16)),
-                child: Text(
-                  dog.nome,
-                  style: TextStyle(fontSize: 26, color: Colors.white),
-                ),
-              ),
+        /// Nesta ordem a foto fica por baixo do nome
+        Container(
+          /// Ao inv[es de Container pode-se usar também o widget Align, porém o Container possui mais recursos
+          alignment: Alignment.topLeft,
+          child: Container(
+            margin: EdgeInsets.all(12),
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+
+                /// O Black45 possui 45% de opacidade
+                color: Colors.black45,
+                borderRadius: BorderRadius.circular(16)),
+            child: Text(
+              dog.nome,
+              style: TextStyle(fontSize: 26, color: Colors.white),
             ),
-          ],
-        );
-      },
+          ),
+        ),
+      ],
     );
   }
 
